a2a7e3f27af27e6b161ce2d299e6b1e74eb75500
Kraizlol <kraizlol@mail.ru>
2024-12-16 17:35:22 +0400

    configure project

---
+++ app/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    application
+    checkstyle
+    jacoco
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.3"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.3")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+application {
+    mainClass = "hexlet.code.App"
+}
